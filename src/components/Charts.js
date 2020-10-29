81258650727a63177aa6d05f7c005408c524ace6
Theo Booth <theorlbooth@googlemail.com>
2020-10-29 18:38:32 +0000

    working carousel

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -4,62 +4,7 @@ import { Link } from 'react-router-dom'
 import Loader from './Loader'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
-
-
-
-
-const navigationArray = [
-  {
-    name: 'Top Worldwide',
-    id: 3155776842,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/f1ac18441ab1dabc94282e4d1d5f4955/264x264-000000-80-0-0.jpg'
-  },
-  {
-    name: 'Top USA',
-    id: 1313621735,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/5db67fb2a5d10d69fe4780dc11b2b174/264x264-000000-80-0-0.jpg'
-  },
-  {
-    name: 'Top Ireland',
-    id: 1313619455,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/6d2f56f6ab811a819527076b76f93d71/264x264-000000-80-0-0.jpg'
-  },
-  {
-    name: 'Top Brazil',
-    id: 1111141961,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/135c4adb3d5115d128c0635adc4ebae5/264x264-000000-80-0-0.jpg'
-  },
-  {
-    name: 'Top Germany',
-    id: 1111143121,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/fbd9767ddb69cf4c43889f8e141f19b2/264x264-000000-80-0-0.jpg'
-  },
-  {
-    name: 'Top France',
-    id: 1109890291,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/b40301f3a462bcad6cc2e75937be05be/264x264-000000-80-0-0.jpg'
-  },
-  {
-    name: 'Top Colombia',
-    id: 1116188451,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/2095e4251bdd07ab8af8269dfa142a40/264x264-000000-80-0-0.jpg'
-  },
-  {
-    name: 'Top Canada',
-    id: 1652248171,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/afd4cd8b44c82dfbc04a0b5a889f95ba/264x264-000000-80-0-0.jpg'
-  },
-  {
-    name: 'Top Mexico',
-    id: 1111142361,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/155600857cc335866d78c03d0bde4809/264x264-000000-80-0-0.jpg'
-  },
-  {
-    name: 'Top Netherlands',
-    id: 1266971851,
-    image: 'https://e-cdns-images.dzcdn.net/images/playlist/b7758fed610ecc6442768a424e23ae8e/264x264-000000-80-0-0.jpg'
-  }
-]
+import navigationArray from './Playlists'
 
 
 
@@ -100,8 +45,8 @@ const Charts = () => {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 5,
-      slidesToSlide: 3
+      items: 6.5,
+      slidesToSlide: 6
     }
   }
 
